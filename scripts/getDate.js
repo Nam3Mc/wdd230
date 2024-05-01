cfc7cc65a36bab59692ef87eb11592bbf05e2240
namemc95 <moralesdreiser5@gmail.com>
2024-05-01 00:31:15 -0500

    done

---
+++ scripts/getDate.js
@@ -0,0 +1,36 @@
+const daysNames = [
+    "Sunday" ,
+    "Monday" ,
+    "Tuesday" ,
+    "Wednesday" ,
+    "Thursday" ,
+    "Friday" , 
+    "Saturday"
+];
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "June",
+    "July",
+    "August",
+    "Spteber",
+    "November",
+    "December"
+];
+
+const todaysdate = new Date();
+const dayName = daysNames[todaysdate.getDay()];
+const monthName = months[todaysdate.getMonth()];
+
+const currentdate = dayName + ", " + todaysdate.getDate() + " " + monthName + ", " + todaysdate.getFullYear();
+
+document.getElementById("currentdate").textContent = currentdate
+
+document.addEventListener("DOMContentLoaded", function () {
+    var lastModified = document.lastModified ;
+    var lastModifiedElement = document.getElementById("lastModified");
+    lastModifiedElement.textContent ="last Modified: " + lastModified ; 
+})
